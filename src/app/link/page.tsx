4907bfb0d03de2ec73bbd44cddba430faf20a702
stien95 <stivenvillegas258@gmail.com>
2024-02-26 12:57:30 -0500

    ALPHA 1.21V

--- src/app/link/page.tsx
+++ src/app/link/page.tsx
@@ -1,3 +1,46 @@
-export default function page({searchParams}) {
-  
+import { redirect } from "next/navigation";
+import { validTypes } from "../create/business/ContactSection";
+
+interface Props {
+  searchParams: {
+    type: keyof typeof validTypes;
+    url: string;
+  };
+}
+
+export default function LinkPage({ searchParams }: Props) {
+  if (searchParams.type === "whatsapp") {
+    const link =
+      searchParams.url.startsWith("https://api.whatsapp.com") ||
+      searchParams.url.startsWith("https://wa.link")
+        ? searchParams.url
+        : `https://api.whatsapp.com/send?phone=${searchParams.url}`;
+    return redirect(link);
+  }
+  if (searchParams.type === "email") {
+    const link = searchParams.url.startsWith("mailto:")
+      ? searchParams.url
+      : `mailto:${searchParams.url}`;
+    return redirect(link);
+  }
+  if (searchParams.type === "phone") {
+    const link = searchParams.url.startsWith("tel:")
+      ? searchParams.url
+      : `tel:${searchParams.url}`;
+    return redirect(link);
+  }
+  if (searchParams.type === "instagram") {
+    const link = searchParams.url.startsWith("https://www.instagram.com/")
+      ? searchParams.url
+      : `https://www.instagram.com/${searchParams.url}`;
+    return redirect(link);
+  }
+  if (searchParams.type === "telegram") {
+    const link = searchParams.url.startsWith("https://www.t.me/")
+      ? searchParams.url
+      : `https://www.t.me/${searchParams.url}`;
+    return redirect(link);
+  } else {
+    return redirect("/");
+  }
 }
